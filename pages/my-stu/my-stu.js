7a8174e9ba7c32dce270e59ff37176fe7466311c
YIdyw <1004652005@qq.com>
2020-09-30 12:09:17 +0800

    版本1.1.3
    增加教师课表

--- pages/my-stu/my-stu.js
+++ pages/my-stu/my-stu.js
@@ -314,6 +314,30 @@ isout(){
   wx.clearStorage()
 },
 
+//查看授权情况
+my_setting() {
+  wx.getSetting({
+    withSubscriptions: true,
+    success (res) {
+      console.log(res.authSetting)
+      // res.authSetting = {
+      //   "scope.userInfo": true,
+      //   "scope.userLocation": true
+      // }
+      console.log(res.subscriptionsSetting)
+      // res.subscriptionsSetting = {
+      //   mainSwitch: true, // 订阅消息总开关
+      //   itemSettings: {   // 每一项开关
+      //     SYS_MSG_TYPE_INTERACTIVE: 'accept', // 小游戏系统订阅消息
+      //     SYS_MSG_TYPE_RANK: 'accept'
+      //     zun-LzcQyW-edafCVvzPkK4de2Rllr1fFpw2A_x0oXE: 'reject', // 普通一次性订阅消息
+      //     ke_OZC_66gZxALLcsuI7ilCJSP2OJ2vWo2ooUPpkWrw: 'ban',
+      //   }
+      // }
+    }
+  })
+},
+
 picture2(){
   wx.scanCode({
     success: (res) => {
@@ -388,6 +412,9 @@ picture2(){
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    //this.my_setting()
+
     let today = new Date();
     let currentYear = today.getFullYear();
     let currentMonth = today.getMonth() + 1;
