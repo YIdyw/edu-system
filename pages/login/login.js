bc9618ca269b1eba35f0502f2fa1a33497c3d9aa
YIdyw <1004652005@qq.com>
2021-05-06 19:20:59 +0800

    输出code测试接口

--- pages/login/login.js
+++ pages/login/login.js
@@ -218,6 +218,7 @@ Page({
           userid: wx.getStorageSync('loginInfo').userid,
           code: res.code
         }
+        console.log("这就是我的code："+res.code);
         code(data2).then(res1 =>{
           if(res1.code==200){
             console.log("code发送成功！")
